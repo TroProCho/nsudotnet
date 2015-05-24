66e6405f2fedf9bcdc5c6ac0625b08d0bcdf0bc3
Alexandr Byzov <Aleksandr.byzov@mail.ru>
2015-05-24 22:46:35 +0600

    Сделана лаба

---
+++ Byzov.Nsudotnet.NumberGuesser/NumberGuesser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            (new NumberGuesser(Console.In, Console.Out)).StartGame();
+        }
+    }
+}
